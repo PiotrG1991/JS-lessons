ef96e8bb7c178aaff2255e78e25a1aeacb1ce611
Piotrek <piotrg3378@wp.pl>
2024-01-11 16:16:09 +0100

    teory

--- src/scripts/async.js
+++ src/scripts/async.js
@@ -32,22 +32,65 @@ const box = document.querySelector('.box')
 //    .catch((noKebab) => {console.log(noKebab)})
  //   .finally(() => console.log('zawsze'));
 
-function animate(element, {top, left}){
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            if (top !== undefined){
-                element.style.top = top;
-            }
+////function animate(element, {top, left}){
+////    return new Promise((resolve) => {
+ ////       setTimeout(() => {
+////            if (top !== undefined){
+////                element.style.top = top;
+////            }
+////
+ ////           if (left !== undefined){
+////                element.style.left = left;
+////            }
+////            resolve();
+////        }, 3000)
+////    });
+////}
 
-            if (left !== undefined){
-                element.style.left = left;
-            }
-            resolve();
-        }, 3000)
-    });
+//animate(box, {left: '400px'})
+//    .then(() => animate(box, {top: '400px'}))
+ //   .then(() => animate(box, {left: '100px'}))
+ //   .then(() => animate(box, {top: '100px'}));
+
+// Promise based - oparte o promisie
+
+//const request = fetch("http://api.nbp.pl/api/exchangerates/rates/A/chf/?format=json")
+//console.log(response);
+//request
+//    .then((response) => response.json())
+//    .then((data) => console.log(data.rates[0].mid))
+//    .catch(() => console.log('Something is no yes!'))
+
+// trzeci sposob
+//async function getData(code){
+//    const response = await fetch(`http://api.nbp.pl/api/exchangerates/rates/A/${code}/?format=json`);
+ //   const data = await response.json();
+ //   return data.rates[0].mid
+//}
+//
+//const showData = async () => {
+//    const result = await getData('eur');
+//    console.log(result)
+//    return result;
+//}
+
+//getData('eur');
+//showData().then((r) => console.log(r));
+
+// this informacje!!!!!!!!!!!!
+
+function createPlayer(name) {
+    return {
+        score: 0,
+        name,
+        updateScore() {
+            player.score++;
+        }
+    }
 }
 
-animate(box, {left: '400px'})
-    .then(() => animate(box, {top: '400px'}))
-    .then(() => animate(box, {left: '100px'}))
-    .then(() => animate(box, {top: '100px'}));
+const player = createPlayer('Janusz K');
+
+console.log(player.score);
+player.updateScore();
+console.log(player.score);
